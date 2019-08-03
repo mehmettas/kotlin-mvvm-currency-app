ceac8ddbf485ab2f3c4f089f50d1142882a5d891
mehmettas <mtas372372@hotmail.com>
2019-08-03 19:06:20 +0300

    constants for logic

--- Cent/app/src/main/java/com/mehmettas/cent/utils/AppConstants.kt
+++ Cent/app/src/main/java/com/mehmettas/cent/utils/AppConstants.kt
@@ -6,4 +6,5 @@ object AppConstants {
     const val API_URL = "https://api.exchangeratesapi.io/"
     const val API_URL_MODEL = "https://api.appnexus.com/currency"
     const val REQUEST_TIMEOUT: Long = 60
+    const val DRAWEBLE_AMOUNT = 4
 }

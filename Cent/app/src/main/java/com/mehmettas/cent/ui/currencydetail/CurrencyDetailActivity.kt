73abb8ecf6e86fa8f2244ea1219a4c43c4ed6560
mehmettas <mtas372372@hotmail.com>
2019-08-06 18:40:20 +0300

    Detail page

---
+++ Cent/app/src/main/java/com/mehmettas/cent/ui/currencydetail/CurrencyDetailActivity.kt
@@ -0,0 +1,21 @@
+package com.mehmettas.cent.ui.currencydetail
+
+import com.mehmettas.cent.ui.base.BaseActivity
+
+class CurrencyDetailActivity: BaseActivity, ICurrencyDetailNavigator {
+
+    override val layoutId: Int?
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+
+    override fun initNavigator() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun initUI() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun initListener() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
---
+++ Cent/app/src/main/java/com/mehmettas/cent/ui/currencydetail/ICurrencyDetailNavigator.kt
@@ -0,0 +1,6 @@
+package com.mehmettas.cent.ui.currencydetail
+
+import com.mehmettas.cent.ui.base.IBaseNavigator
+
+interface ICurrencyDetailNavigator: IBaseNavigator {
+}
